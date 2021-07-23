10e11855e02bfbd9dc7028acb3ad4cc4012644f7
Justin <hurstjustina@gmail.com>
2021-07-23 10:43:28 -0400

    message in progress

---
+++ client/src/Reviews_Ratings/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+
+  render() {
+    return (
+      <>
+        <h1>
+          Hello Ratings and Reviews
+        </h1>
+      </>
+    );
+  }
+}
+
+export default Main;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Main from '../src/Reviews_Ratings/main.jsx';
 
 
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Main />, document.getElementById('app'));
+//remember to switch back to App
