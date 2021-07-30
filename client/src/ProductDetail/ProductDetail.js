e9c62f736ea0046db243706bb664a53b1862cc6a
swizzlestix09 <angely.rodriguez@gmail.com>
2021-07-29 22:29:28 -0400

    CMON

---
+++ client/src/ProductDetail/ProductDetail.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Carosel from './Image_Gallery/Carosel';
+import photos from './Image_Gallery/CaroselFixtures';
+import config from './config';
+
+class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    var data = '';
+    console.log('MOUNT! ', this.props.data.productID)
+    var config = {
+      method: 'get',
+      url: `http://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${this.props.data.productID}/styles`,
+      headers: {
+        'Authorization': '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'
+      },
+      data : data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log(this.props.data.productID);
+    return (
+      <div>
+        <Carosel />
+      </div>
+    );
+  }
+}
+
+export default ProductDetail;
