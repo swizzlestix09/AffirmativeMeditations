675bc30707aad264c62e232deb33e7a6a93031f3
swizzlestix09 <angely.rodriguez@gmail.com>
2021-08-02 17:11:13 -0400

    adding files

---
+++ client/src/ProductDetail/ProductDesc.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Carosel from './Carosel';
+import config from './../../../config';
+import ImageGallery from './ImageGallery';
+import ProductInformation from './ProductInformation';
+
+const ProductDesc = (props) => {
+  console.log('proddesc ', props.features);
+  return (
+    <div className="productDesc">
+      <span className="slogan">{props.slogan}</span>
+      <div className="description">{props.desc}</div>
+      <ul className="features">
+        {Array.isArray(props.features)
+          ? props.features.map( (feat, idx) => { return <li className="feat" key={idx.toString()}>{feat.feature} : {feat.value}</li>; } )
+          : null}
+      </ul>
+    </div>
+  );
+};
+export default ProductDesc;
