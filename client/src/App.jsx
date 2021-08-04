870fc2a9900a89179ded58bd45b91ddd3162e3b2
7u1ian <jsn.chn@outlook.com>
2021-08-03 17:09:31 -0700

    Complete modal bare minimum functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ class App extends React.Component {
     this.state = {
       productID: sampleData.id,
       productName: sampleData.name,
+      productCategory: sampleData.category,
+      productPrice: sampleData.default_price,
       productFeatures: sampleData.features
     };
     this.changeState = this.changeState.bind(this);
@@ -29,8 +31,15 @@ class App extends React.Component {
         Our state now exists.<br>
         </br>Product ID: {this.state.productID}<br>
         </br>Product Name: {this.state.productName}<br>
+        </br>Product Features: {this.state.features}<br>
         </br><Main /><br>
-        </br><RelatedItems productID={this.state.productID} changeState={this.changeState} /><br></br>
+        </br><RelatedItems
+                productID={this.state.productID}
+                productName={this.state.productName}
+                productCategory={this.state.productCategory}
+                productPrice={this.state.productPrice}
+                productFeatures={this.state.productFeatures}
+                changeState={this.changeState} /><br></br>
       </>
     );
   }
