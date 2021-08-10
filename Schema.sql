ddfd52c0b8d6cb60ded610c93cd29310b571a123
swizzlestix09 <angely.rodriguez@gmail.com>
2021-08-10 17:25:21 -0400

    added pg & modified schema

--- Schema.sql
+++ Schema.sql
@@ -17,11 +17,11 @@ USE productsDetails;
 -- Table 'Features'
 --
 -- ---
-CREATE TABLE `Features` (
-  `product_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  `Feature` MEDIUMTEXT(50) NULL DEFAULT 'NOT NULL',
-  `value` VARCHAR(120) NOT NULL DEFAULT 'NOT NULL',
-  PRIMARY KEY (`product_id`)
+CREATE TABLE Features (
+  product_id INT NOT NULL,
+  feature VARCHAR ( 50 ),
+  value VARCHAR ( 120 ),
+  PRIMARY KEY ( product_id )
 );
 
 -- ---
@@ -29,14 +29,14 @@ CREATE TABLE `Features` (
 --
 -- ---
 
-CREATE TABLE `Product Info` (
-  `product_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  `name` VARCHAR(120) NOT NULL DEFAULT 'NOT NULL',
-  `slogan` VARCHAR(240 ) NOT NULL DEFAULT 'NOT NULL',
-  `description` VARCHAR(540) NULL DEFAULT NULL,
-  `category` MEDIUMTEXT(50) NOT NULL DEFAULT 'NULL',
-  `default_price` VARCHAR(7) NULL DEFAULT 'NOT NULL',
-  PRIMARY KEY (`product_id`)
+CREATE TABLE ProductInfo (
+  product_id INTEGER,
+  name VARCHAR(120),
+  slogan VARCHAR(240 ),
+  description VARCHAR(540),
+  category VARCHAR(50),
+  default_price VARCHAR(7),
+  PRIMARY KEY (product_id)
 );
 
 -- ---
@@ -44,14 +44,14 @@ CREATE TABLE `Product Info` (
 --
 -- ---
 
-CREATE TABLE `styles` (
-  `product_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  `style_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  `name` VARCHAR(50) NOT NULL DEFAULT 'NOT NULL',
-  `original_price` VARCHAR(7) NULL DEFAULT NULL,
-  `sale_price` VARCHAR(7) NULL DEFAULT NULL,
-  `default?` TINYINT(1) NOT NULL DEFAULT NOT NULL,
-  PRIMARY KEY (`product_id`)
+CREATE TABLE styles (
+  product_id INT NOT NULL,
+  style_id INT  NOT NULL,
+  name VARCHAR(50),
+  original_price VARCHAR(7),
+  sale_price VARCHAR(7),
+  isdefault boolean,
+  PRIMARY KEY (product_id)
 );
 
 -- ---
@@ -59,10 +59,10 @@ CREATE TABLE `styles` (
 --
 -- ---
 
-CREATE TABLE `related_products` (
-  `product_id` INTEGER(10) NOT NULL DEFAULT NOT_NULL,
-  `related_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  PRIMARY KEY (`product_id`)
+CREATE TABLE related_products (
+  product_id INT NOT NULL,
+  related_id INT NOT NULL,
+  PRIMARY KEY (product_id)
 );
 
 -- ---
@@ -70,60 +70,60 @@ CREATE TABLE `related_products` (
 --
 -- ---
 
-CREATE TABLE `photos` (
-  `product_id` INTEGER(10) NULL AUTO_INCREMENT DEFAULT NOT NULL,
-  `thumbnail_url` MEDIUMTEXT NULL DEFAULT NULL,
-  `url` MEDIUMTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`product_id`)
-);
+-- CREATE TABLE photos (
+--   product_id INT NOT NULL,
+--   thumbnail_url MEDIUMTEXT NULL DEFAULT NULL,
+--   url MEDIUMTEXT NULL DEFAULT NULL,
+--   PRIMARY KEY (product_id)
+-- );
 
 -- ---
 -- Table 'skus'
 --
 -- ---
 
-CREATE TABLE `skus` (
-  `product_id` INTEGER(10) NOT NULL DEFAULT NOT NULL,
-  `sku_id` VARCHAR(10) NULL DEFAULT 'NOT NULL',
-  `quantity` INTEGER(5) NULL DEFAULT NOT NULL,
-  `size` VARCHAR(10) NULL DEFAULT 'NOT NULL',
-  PRIMARY KEY (`product_id`)
+CREATE TABLE skus (
+  product_id INT NOT NULL,
+  sku_id VARCHAR(10) NOT NULL,
+  quantity INT NOT NULL,
+  size VARCHAR(10) NOT NULL,
+  PRIMARY KEY (product_id)
 );
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `Product Info` ADD FOREIGN KEY (product_id) REFERENCES `Features` (`product_id`);
-ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `Product Info` (`product_id`);
-ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `skus` (`product_id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (product_id) REFERENCES `styles` (`product_id`);
+ALTER TABLE ProductInfo ADD FOREIGN KEY (product_id) REFERENCES Features (product_id);
+ALTER TABLE styles ADD FOREIGN KEY (product_id) REFERENCES Product Info (product_id);
+ALTER TABLE styles ADD FOREIGN KEY (product_id) REFERENCES skus (product_id);
+
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `Features` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Product Info` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `styles` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `related_products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `photos` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `skus` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Features ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Product Info ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE styles ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE related_products ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE photos ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE skus ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `Features` (`product_id`,`Feature`,`value`) VALUES
+-- INSERT INTO Features (product_id,Feature,value) VALUES
 -- ('','','');
--- INSERT INTO `Product Info` (`product_id`,`name`,`slogan`,`description`,`category`,`default_price`) VALUES
+-- INSERT INTO Product Info (product_id,name,slogan,description,category,default_price) VALUES
 -- ('','','','','','');
--- INSERT INTO `styles` (`product_id`,`style_id`,`name`,`original_price`,`sale_price`,`default?`) VALUES
+-- INSERT INTO styles (product_id,style_id,name,original_price,sale_price,default?) VALUES
 -- ('','','','','','');
--- INSERT INTO `related_products` (`product_id`,`related_id`) VALUES
+-- INSERT INTO related_products (product_id,related_id) VALUES
 -- ('','');
--- INSERT INTO `photos` (`product_id`,`thumbnail_url`,`url`) VALUES
+-- INSERT INTO photos (product_id,thumbnail_url,url) VALUES
 -- ('','','');
--- INSERT INTO `skus` (`product_id`,`sku_id`,`quantity`,`size`) VALUES
+-- INSERT INTO skus (product_id,sku_id,quantity,size) VALUES
 -- ('','','','');
 
