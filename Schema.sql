10793e369bc5465852d4a6a881eec3b50a02cfd0
swizzlestix09 <angely.rodriguez@gmail.com>
2021-08-09 20:04:07 -0400

    schema created

--- Schema.sql
+++ Schema.sql
@@ -57,14 +57,3 @@ CREATE TABLE related (
 
   PRIMARY KEY (product_id)
   );
-/*  Execute this file from the command line by typing:
- *    mysql -u <USER> < schema.sql
- *    OR
- *    mysql -u <USER> -p < schema.sql
- *  For example, on a pairing station, it'll be
- *    mysql -u student -p < schema.sql
- *  and then you'll have to enter the password, student
- *  On your personal computer, if you haven't set up
- *  a password, it'll be
- *    mysql -u root -p < schema.sql
-*/
