37d128ad19012b9937b16cb5747a7547ae2edc52
swizzlestix09 <angely.rodriguez@gmail.com>
2021-08-23 18:18:49 -0400

    React Up, Installed dependancies

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hi</p>
+      </div>
+    );
+  }
+}
+
+export default App;
