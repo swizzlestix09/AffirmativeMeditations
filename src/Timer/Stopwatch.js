f77372d66555b8c138fc5dcc254851f5130e6e4a
swizzlestix09 <angely.rodriguez@gmail.com>
2021-08-25 12:21:26 -0400

    working on stopwatch

--- src/Timer/Stopwatch.js
+++ src/Timer/Stopwatch.js
@@ -9,44 +9,57 @@ class Stopwatch extends React.Component {
       minutes: 0,
       seconds: 0,
       paused: false,
+      intervalID: 0,
     };
+
     this.pauseTimer = this.pauseTimer.bind(this);
     this.setTimer = this.setTimer.bind(this);
     this.tick = this.tick.bind(this);
     this.pausedOrSrt = this.pausedOrSrt.bind(this);
     this.stop = this.stop.bind(this);
   }
+  componenetDidUpdate(prevState) {
+    if (prevState.seconds !== this.state.seconds && this.state.minutes > 0) {
+      this.tick();
+    }
+  }
 
   changeSeconds() {
-    let sec = this.state.seconds;
-    if (this.state.seconds >= 0) {
-      this.setState({ seconds: sec-- });
+    let sec;
+
+    if (this.state.seconds === 0 && this.state.minutes > 0) {
+      sec = 59;
+      let mins = this.state.minutes - 1;
+      this.setState({ minutes: mins, seconds: sec });
+    } else if (this.state.seconds > 0 && this.state.minutes > -1) {
+      sec = this.state.seconds - 1;
+      this.setState({ seconds: sec });
+    } else if (this.state.seconds === 0 && this.state.minutes === 0) {
+      let intervalID = this.state.intervalID;
+      clearInterval(this.setState({ intervalID }));
     }
+    this.changeMinutes();
   }
 
   changeMinutes() {
-    let min = this.state.minutes;
-    if (this.state.seconds === 0 && this.state.minutes > 0) {
-      this.setState({ minutes: min-- });
-    }
+    console.log("after change ", this.state.minutes, this.state.seconds);
   }
 
   tick() {
-    console.log('ayyyyyy')
+    console.log("ayyyyyy");
+    const intervalID = setInterval(this.changeSeconds(), 1000);
+    this.setState({ intervalID });
   }
 
-  stop(){
-    console.log('eeeeyyyyy')
+  stop() {
+    console.log("eeeeyyyyy");
   }
 
   pausedOrSrt() {
     let ispaused = this.state.paused;
     this.setState({ paused: !ispaused });
-
     this.state.paused === true ? this.stop() : this.tick();
   }
-
-
 
   pauseTimer() {}
 
@@ -55,6 +68,7 @@ class Stopwatch extends React.Component {
   }
 
   render() {
+    console.log("before change ", this.state.minutes, this.state.seconds);
     return (
       <div>
         <p>
